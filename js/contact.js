71578feb36258bc4d1de2051613ac5cd178deb06
isavel-papa <avelpapa8@yahoo.com>
2019-03-14 10:43:19 -0700

    login and contact work

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,9 @@
-
-
-function goToHome() {
-    console.log('from goToHome...');
-    window.location.href = 'index.html';
+var Contact = /** @class */ (function () {
+    function Contact() {
     }
-    
+    Contact.prototype.goToHome = function () {
+        window.location.href = 'index.html';
+    };
+    return Contact;
+}());
+var contact = new Contact();
